302dfb60cc49177d37f6f6527e7275583d296653
Bmax2 <hmark3200@gmail.com>
2023-03-16 13:09:49 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <studio.h>
+
 /**
- * main  - Main function
- *
- * Return: 0
+ *main - Entry Point
+ *Return: Always 0(Success)
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
