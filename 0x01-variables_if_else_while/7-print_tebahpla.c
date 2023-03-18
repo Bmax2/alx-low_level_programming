8c6e102b2dea28abab0d7588dfa1d4299e253005
Bmax2 <hmark3200@gmail.com>
2023-03-17 18:14:56 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'z' ; i > = 'a' ; i--)
+		putchar9(i);
+	putchar(''\n');
+	return (0);
+}
